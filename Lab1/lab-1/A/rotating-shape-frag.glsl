12ec72b63d38bcbbb5aa9a3f4fe2209495d6d32d
3RFUNn <68325981+3RFUNn@users.noreply.github.com>
2024-10-03 21:53:56 +0100

    Update rotating-shape-frag.glsl

--- Lab1/lab-1/A/rotating-shape-frag.glsl
+++ Lab1/lab-1/A/rotating-shape-frag.glsl
@@ -2,10 +2,21 @@ precision mediump float;
 
 // A4: ADD CODE HERE
 
+varying lowp vec4 colour_var;
+
+
 void main()
 {
     // A2 & A4: MODIFY BELOW
 
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = colour_var;
+
+
+    //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+
+    // vec3 red = vec3(1.0, 0.0, 0.0);
+    // vec3 green = vec3(0.0, 1.0, 0.0);
+    // vec4 add = vec4(red + green, 1.0);
+    // gl_FragColor = add;
 }
 
